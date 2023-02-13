1e9363baec242810629f48e0aeafb01087a647e2
NguyenQuy03 <huyquy2003@gmail.com>
2023-02-14 00:25:29 +0700

    Update Insert SQL data

--- database/insert_data.sql
+++ database/insert_data.sql
@@ -1,27 +1,15 @@
 use `spring-boot-ecommerce`;
 
 INSERT INTO role(code, name) VALUE("MANAGER", "manager");
-INSERT INTO role(code, name) VALUE("AUDITADMIN", "audit-admin");
 INSERT INTO role(code, name) VALUE("BUYER", "buyer");
 INSERT INTO role(code, name) VALUE("SELLER", "seller");
 
-INSERT INTO account(username, password, fullname, status) VALUE("huyquy", "$2a$12$t.e6fMXk7MdEjy9KzcYACe12RsU2vCLO4WH2RRAGBBGQ9mlk3Mdzu", "Nguyễn Huy Quý", true);	
-INSERT INTO account(username, password, fullname, status) VALUE("audit", "$2a$12$t.e6fMXk7MdEjy9KzcYACe12RsU2vCLO4WH2RRAGBBGQ9mlk3Mdzu", "Trần Danh A", true);
+INSERT INTO account(username, password, fullname, status) VALUE("huyquy", "$2a$10$22dP2Tp4lh19L9qRpn0Ye.fXuahHtYDFgpJYpz3iA2SIqw75cuAJO", "Nguyễn Huy Quý", true);	
 INSERT INTO account(username, password, fullname, status) VALUE("buyer", "$2a$12$t.e6fMXk7MdEjy9KzcYACe12RsU2vCLO4WH2RRAGBBGQ9mlk3Mdzu", "Nguyễn Văn B", true);
 INSERT INTO account(username, password, fullname, status) VALUE("menClothes", "$2a$12$t.e6fMXk7MdEjy9KzcYACe12RsU2vCLO4WH2RRAGBBGQ9mlk3Mdzu", "Nguyễn Tuấn S", true);
 INSERT INTO account(username, password, fullname, status) VALUE("womenClothes", "$2a$12$t.e6fMXk7MdEjy9KzcYACe12RsU2vCLO4WH2RRAGBBGQ9mlk3Mdzu", "Nguyễn Minh S", true);
 
-
-INSERT INTO accounts_roles(account_id, role_id) VALUE(1, 1);
-INSERT INTO accounts_roles(account_id, role_id) VALUE(2, 2);
-INSERT INTO accounts_roles(account_id, role_id) VALUE(3, 3);
-INSERT INTO accounts_roles(account_id, role_id) VALUE(4, 4);
-INSERT INTO accounts_roles(account_id, role_id) VALUE(5, 4);
-
-/*https://drive.google.com/drive/folders/1f1ee6tIXzgewkF9D1s2rAwy632oUiVXL?usp=sharing*/
-INSERT INTO category(code, name) VALUE("women-clothes", "Women Clothes");
-INSERT INTO category(code, name) VALUE("men-clothes", "Men Clothes");
-INSERT INTO category(code, name) VALUE("beauty", "Beauty");
-INSERT INTO category(code, name) VALUE("health", "Health");
-INSERT INTO category(code, name) VALUE("mobile-gadgets", "Mobile & Gadgets");
-INSERT INTO category(code, name) VALUE("books-magazines", "Books & Magazines");
+INSERT INTO accounts_roles(account_id, role_code) VALUE(1, "MANAGER");
+INSERT INTO accounts_roles(account_id, role_code) VALUE(2, "BUYER");
+INSERT INTO accounts_roles(account_id, role_code) VALUE(3, "SELLER");
+INSERT INTO accounts_roles(account_id, role_code) VALUE(4, "SELLER");
