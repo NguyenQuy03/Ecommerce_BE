5c38df12759e8b4f8e0bb16957189d79ad120124
NguyenQuy03 <huyquy2003@gmail.com>
2023-07-25 16:17:15 +0700

    Refactor Codebase P1
    using MongoDB instead of MySQL

--- src/main/resources/static/seller/js/addOrEdit.js
+++ src/main/resources/static/seller/js/addOrEdit.js
@@ -1,9 +1,25 @@
 
+const saveBtn = $("#saveBtn");
+const APIProductUrl = "/api/seller/product";
+const form = $("#formSubmit");
+const data = {};
+
 $(document).ready(function() {
-	$("#imageFile").change(function() {
+	$("#imageBase64").change(function() {
 		showPreviewImage(this);
 	})
-	
+	if($("#id")[0].value === '') {
+        addSpecRow();
+	} else {
+	    $("#imageBase64")[0].setAttribute("selected", "");
+	    $("#imageBase64")[0].removeAttribute("required");
+	}
+
+    ClassicEditor
+            .create( document.querySelector( '#details') )
+            .catch( error => {
+                console.error( error );
+            } );
 })
 
 function showPreviewImage(imageFile) {
@@ -11,9 +27,102 @@ function showPreviewImage(imageFile) {
 	var reader = new FileReader();
 
 	reader.onload = function(e) {
-		$("#previewImage").attr('src', e.target.result);
+		$(".previewImage").attr('src', e.target.result);
+		data["imageBase64"] = e.target.result;
 	}
 
-	reader.readAsDataURL(file);
+    if(file) {
+	   reader.readAsDataURL(file);
+    }
 }
+
+$("#row-adder").click(function (e) {
+    e.preventDefault();
+    addSpecRow();
+});
+
+function addSpecRow(){
+    newRowAdd =
+        '<div class="spec-el spec-row col-sm-10 d-flex spec-fit">' +
+            '<input required type="text" aria-label="Key" class="spec-key input-group-text">' +
+            '<input required type="text" aria-label="Value" class="spec-value form-control">' +
+            '<button class="delete-spec-btn btn btn-danger" type="button">' +
+                '<i class="bi bi-trash"></i>' +
+            '</button>' +
+        '</div>';
+
+    $('#new-input').append(newRowAdd);
+}
+
+$("body").on("click", ".delete-spec-btn", function () {
+    $(this).parents(".spec-row").remove();
+})
+
+
+saveBtn.click(function(e) {
+    e.preventDefault();
+    const formData = form.serializeArray();
+    const specEls = document.querySelectorAll(".spec-el")
+
+    const specData = [];
+    specEls.forEach(specEl => {
+        let tmp = {};
+        tmp["k"] = specEl.children[0].value;
+        tmp["v"] = specEl.children[1].value;
+        specData.push(tmp);
+    })
+    data["specifications"] = specData;
+
+    $.each(formData, function (i, v) {
+        data[v.name] = v.value;
+    });
+
+    let isValidForm = form[0].checkValidity();
+
+    if(isValidForm && $("#id")[0].value === '') {
+        addProduct(data);
+    } else if (isValidForm) {
+        updateProduct(data);
+    } else {
+        form[0].classList.add("was-validated");
+    }
+
+})
+
+
+function addProduct(data) {
+    $.ajax({
+        url: APIProductUrl,
+        type: "POST",
+        contentType: "application/json",
+        processData: false,
+        cache: false,
+        data: JSON.stringify(data),
+        success: function() {
+            window.location.href = "/seller/product/list/all?page=1&size=2&message=addSucceed";
+        },
+        error: function(e) {
+            console.log("error" + e)
+        }
+    });
+}
+
+function updateProduct(data) {
+    $.ajax({
+        url: APIProductUrl,
+        type: "PUT",
+        contentType: "application/json",
+        processData: false,
+        cache: false,
+        data: JSON.stringify(data),
+        success: function() {
+            window.location.href = "/seller/product/list/all?page=1&size=2&message=addSucceed";
+        },
+        error: function(e) {
+            console.log("error" + e)
+        }
+    });
+}
+
+
 
